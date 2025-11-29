9ed0217ed65b011b689fd015739fdcb09711cd15
eelishap <eeli55526@gmail.com>
2025-11-29 17:48:57 +0545

    fix: update mysql script

--- scripts/mysql.sh
+++ scripts/mysql.sh
@@ -19,20 +19,44 @@ NR_REQUESTS=1000
 TABLE_SIZE=1000000
 RESULTS=mysql.txt
 
+mysql_root_pw="kvm"
+
 function prepare() {
-	mysql -u root --password=kvm < create_db.sql
-	sysbench --test=oltp --oltp-table-size=$TABLE_SIZE --mysql-password=kvm prepare
+    mysql -u root -p${mysql_root_pw} < create_db.sql
+    sysbench /usr/share/sysbench/oltp_read_write.lua \
+        --db-driver=mysql \
+        --mysql-host=$TARGET_IP \
+        --mysql-user=sysbench \
+        --mysql-password=${mysql_root_pw} \
+        --mysql-db=sysbench \
+        --table-size=$TABLE_SIZE \
+        prepare
 }
 
 function cleanup() {
-	sysbench --test=oltp --mysql-password=kvm cleanup
-	mysql -u root --password=kvm < drop_db.sql
+    sysbench /usr/share/sysbench/oltp_read_write.lua \
+        --db-driver=mysql \
+        --mysql-host=$TARGET_IP \
+        --mysql-user=sysbench \
+        --mysql-password=${mysql_root_pw} \
+        --mysql-db=sysbench \
+        cleanup
+    mysql -u root -p${mysql_root_pw} < drop_db.sql
 }
 
 function run() {
-	sysbench --test=oltp $REQ --oltp-table-size=$TABLE_SIZE --num-threads=$num_threads --mysql-host=$TARGET_IP --mysql-password=kvm run | tee \
-	>(grep 'total time:' | awk '{ print $3 }' | sed 's/s//' >> $RESULTS)
+    sysbench /usr/share/sysbench/oltp_read_write.lua $REQ \
+        --db-driver=mysql \
+        --mysql-host=$TARGET_IP \
+        --mysql-user=sysbench \
+        --mysql-password=${mysql_root_pw} \
+        --mysql-db=sysbench \
+        --table-size=$TABLE_SIZE \
+        --num-threads=$num_threads \
+        run | tee \
+        >(grep 'total time:' | awk '{ print $3 }' | sed 's/s//' >> $RESULTS)
 }
+
 
 if [[ "$TARGET_IP" != "localhost" && ("$ACTION" == "prep" || "$ACTION" == "cleanup") ]]; then
 	echo "prep and cleanup actions can only be run on the db server" >&2
@@ -47,7 +71,7 @@ elif [[ "$ACTION" == "run" ]]; then
 	source exits.sh mysql
 	start_measurement
 
-	for num_threads in 200; do
+	for num_threads in 100; do
 		echo -e "$num_threads threads:\n---" >> $RESULTS
 		for i in `seq 1 $REPTS`; do
 			ssh $TEST_USER@$TARGET_IP "pushd ${CMD_PATH};sudo ./mysql.sh prep"
@@ -61,7 +85,7 @@ elif [[ "$ACTION" == "run" ]]; then
 	save_stat
 
 elif [[ "$ACTION" == "cleanup" ]]; then
-	# We will do a lazy-cleanup.
+	#We will do a lazy-cleanup.
 	service mysql stop
 else
 	usage
